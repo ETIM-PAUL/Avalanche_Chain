2639418f4c6e7aad5f4dcb580d19d422e761b503
etim <etimpaul22@gmail.com>
2024-03-31 10:33:19 +0100

    set up tokenevm and tested functionalities

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Joe Tutorial Hyperchain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "JOE_TOKEN"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JOE"
 )
 
 var ID ids.ID
